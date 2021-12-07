7ca112775f2a234ba71aa62cc23b87667250e0f7
Peter Weigman <phweigman@gmail.com>
2021-12-06 21:30:02 -0500

    new client flow up to and including client data input

---
+++ client/src/actions/data.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
+} from './types';
+
+
+//add client
+export const addClient = async ({ firstName, lastName, age, gender, height, weight, standingReach }) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-auth-token': localStorage.getItem('token')
+        }
+    }
+
+    const body = JSON.stringify({ firstName, lastName, age, gender, height, weight, standingReach });
+
+    try {
+        const res = await axios.post('/api/clients', body, config);
+
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
--- client/src/components/main/NewClientInfo.js
+++ client/src/components/main/NewClientInfo.js
@@ -3,13 +3,118 @@ import { Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { login } from '../../actions/auth';
+import { addClient } from '../../actions/data';
 
 
 const NewClientInfo = ({ login, isAuthenticated, clientTypeState, setClientTypeState }) => {
+    const [clientFormState, setClientFormState] = useState({ firstName: '', lastName: '', Age: '', Gender: '', Height: '', Weight: '', StandingReach: '' });
+    const [clientState, setClientState] = useState('');
 
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    return <h1>New</h1>
+        setClientFormState({
+            ...clientFormState,
+            [name]: value,
+        });
+    };
+    const onSubmit = async e => {
+        e.preventDefault();
+        const client = await addClient(clientFormState);
+        setClientState(client.id);
+    }
+
+    if (clientState) {
+        return <Assessment />
+    }
+    return (
+        <Fragment>
+            <div className="row">
+                <form onSubmit={e => onSubmit(e)} className="col s10 offset-s1">
+                    <div className="input-field col s12">
+                        <label htmlFor="firstName" className="active">First Name</label>
+                        <input
+                            className="validate text-input"
+                            name="firstName"
+                            id="firstName"
+                            type="text"
+                            value={clientFormState.firstName}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="lastName" className="active">Last Name</label>
+                        <input
+                            className="validate text-input"
+                            name="lastName"
+                            id="lastName"
+                            type="text"
+                            value={clientFormState.lastName}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="age" className="active">Age</label>
+                        <input
+                            className="validate text-input"
+                            name="age"
+                            id="age"
+                            type="text"
+                            value={clientFormState.age}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="gender" className="active">Gender</label>
+                        <input
+                            className="validate text-input"
+                            name="gender"
+                            id="gender"
+                            type="text"
+                            value={clientFormState.gender}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="height" className="active">Height (inches)</label>
+                        <input
+                            className="validate text-input"
+                            name="height"
+                            id="height"
+                            type="text"
+                            value={clientFormState.height}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="weight" className="active">Weight (lbs)</label>
+                        <input
+                            className="validate text-input"
+                            name="weight"
+                            id="weight"
+                            type="text"
+                            value={clientFormState.weight}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="input-field col s12">
+                        <label htmlFor="standingReach" className="active">Standing Reach (inches)</label>
+                        <input
+                            className="validate text-input"
+                            name="standingReach"
+                            id="standingReach"
+                            type="text"
+                            value={clientFormState.standingReach}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button className="btn waves-effect waves-light col s12" type="submit" name="sign-in">Next
+                    </button>
+                </form>
+            </div>
+        </Fragment>
+    );
 }
 NewClientInfo.propTypes = {
     login: PropTypes.func.isRequired,
@@ -20,4 +125,4 @@ const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, { login })(NewClientInfo);
+export default connect(mapStateToProps)(NewClientInfo);
